c3f9fbee838dddeb73075771e0614cc5016b278a
Brainfeed-1996 <121355278+Brainfeed-1996@users.noreply.github.com>
2024-09-08 00:43:27 +0200

    Create B.c

---
+++ B.c
@@ -0,0 +1,24 @@
+/*
+B – Supprimer les maillons contenant des éléments impairs
+*/
+
+void deleteOddNodes(struct Node** head) {
+    struct Node* current = *head;
+    struct Node* prev = NULL;
+
+    while (current != NULL) {
+        if (current->data % 2 != 0) {
+            if (prev == NULL) {
+                *head = current->next;
+            } else {
+                prev->next = current->next;
+            }
+            struct Node* temp = current;
+            current = current->next;
+            free(temp);
+        } else {
+            prev = current;
+            current = current->next;
+        }
+    }
+}
